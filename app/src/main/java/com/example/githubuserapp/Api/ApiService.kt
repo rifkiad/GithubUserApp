ce9d20ea05ea32a67a46821bf9d4cbf7f06fafba
rifkiaditya <rifkiaditya24545@gmail.com>
2023-04-11 07:35:31 +0700

    add githubapp

--- app/src/main/java/com/example/githubuserapp/Api/ApiService.kt
+++ app/src/main/java/com/example/githubuserapp/Api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.githubuserapp.Api
 
 import com.example.githubuserapp.GithubResponse
+import com.example.githubuserapp.ItemsItem
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -10,14 +11,24 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("/search/users?")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUser(
         @Query("q") q: String
     ): Call<GithubResponse>
 
     @GET("/users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<DetailResponse>
+
+    @GET("/users/{username}/followers")
+    suspend fun getFollowerUser(
+        @Path("username") username: String
+    ): List<ItemsItem>
+
+    @GET("/users/{username}/following")
+    suspend fun getFollowingUser(
+        @Path("username") username: String
+    ): List<ItemsItem>
 }
---
+++ app/src/main/java/com/example/githubuserapp/ListFollower.kt
@@ -0,0 +1,11 @@
+package com.example.githubuserapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListFollower : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_follower)
+    }
+}
